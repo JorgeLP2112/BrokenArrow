d171b23ba9890ecd8e8d10c8d8762844d3572d92
Jorge Martínez <69826862+JorgeLP2112@users.noreply.github.com>
2024-02-14 14:41:04 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -105,6 +105,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
+              </a>
               </main>
               </>
   );
