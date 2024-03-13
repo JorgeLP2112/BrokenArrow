213be37be386b805de9896a76a54b5b684489609
jorgelp2112 <jorgealexmtz35@gmail.com>
2024-03-13 13:23:00 -0600

    Minor changes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -29,7 +29,7 @@ export const authOptions = {
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        username: { label: "Username", type: "text", placeholder: "example" },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
