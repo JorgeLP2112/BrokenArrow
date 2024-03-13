624549743d0cb7b608a760b5073a71b44f2a34e2
Jose Kaleb Ruelas Loo <L20550387@chihuahua2.tecnm.mx>
2024-03-13 12:54:53 -0600

    Add Login/Logout button Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,35 +1,20 @@
 import Image from "next/image";
-import { AiOutlineHome } from "react-icons/ai";
-import { BsPeople } from "react-icons/bs";
-import { TiContacts } from "react-icons/ti";
-import { FiMail } from "react-icons/fi";
-import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+
 import Link from "next/link";
 import { useContext } from "react";
 import { SidebarContext } from "@/context/SidebarContext";
 import { useRouter } from "next/router";
-
-const sidebarItems = [
-    {
-        name: "Home",
-        href: "/",
-        icon: AiOutlineHome,
-    },
-    {
-        name: "Iniciar sesión",
-        href: "api/auth/signin",
-        icon: BsPeople,
-    },
-    {
-        name: "Registrarse",
-        href: "/singup",
-        icon: FiMail,
-    }
-];
+import { useSession, signIn, signOut } from "next-auth/react";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+import { CiLogout } from "react-icons/ci";
+import { AiOutlineHome } from "react-icons/ai";
+import { BsPeople } from "react-icons/bs";
+import { FiMail } from "react-icons/fi";
 
 const Sidebar = () => {
     const router = useRouter();
     const { isCollapsed, toggleSidebarcollapse } = useContext(SidebarContext);
+    const { data: session } = useSession();
 
     return (
         <div className="sidebar__wrapper">
@@ -45,29 +30,63 @@ const Sidebar = () => {
                         src="/logo.jpg"
                         alt="logo"
                     />
-                    <p className="sidebar__logo-name">FitBook</p>
+                    <p className="sidebar__logo-name">JKB Jobs</p>
                 </div>
                 <ul className="sidebar__list">
-                    {sidebarItems.map(({ name, href, icon: Icon }) => {
-                        return (
-                            <li className="sidebar__item" key={name}>
+                    <li className="sidebar__item">
+                        <Link
+                            className={`sidebar__link ${router.pathname === "/" ? "sidebar__link--active" : ""
+                                }`}
+                            href="/"
+                        >
+                            <span className="sidebar__icon">
+                                <AiOutlineHome />
+                            </span>
+                            <span className="sidebar__name">Inicio</span>
+                        </Link>
+                    </li>
+
+
+                    {session ? (
+                        <>
+                            <li className="sidebar__item">
+
+                                <p className="sidebar__link">Signed in as {session.user.name}
+                                </p>
+                                <button className="sidebar__link" onClick={() => signOut()}>
+                                    <span className="sidebar__icon">
+                                        <CiLogout />
+                                    </span>
+                                    Sign out
+                                </button>
+                            </li>
+                        </>
+                    ) : (
+                        <>
+                            <li className="sidebar__item">
+
+                                <button className="sidebar__link" onClick={() => signIn()}>
+                                    <span className="sidebar__icon">
+                                        <BsPeople />
+                                    </span>
+                                    Iniciar Sesion
+                                </button>
+                            </li>
+                            <li className="sidebar__item">
                                 <Link
-                                    className={`sidebar__link ${router.pathname === href ? "sidebar__link--active" : ""
+                                    className={`sidebar__link ${router.pathname === "/singup" ? "sidebar__link--active" : ""
                                         }`}
-                                    href={href}
+                                    href="/singup"
                                 >
                                     <span className="sidebar__icon">
-                                        <Icon />
+                                        <FiMail />
                                     </span>
-                                    <span className="sidebar__name">{name}</span>
-
+                                    <span className="sidebar__name">Registrarse</span>
                                 </Link>
-
                             </li>
-                        );
-                    })}
+                        </>
+                    )}
                 </ul>
-
             </aside>
         </div>
     );
