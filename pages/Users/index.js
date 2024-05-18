59b0a354603b1d9440765b49022e896defdde0a0
Jose Kaleb Ruelas Loo <L20550387@chihuahua2.tecnm.mx>
2024-05-18 15:39:15 -0600

    Deleted Users/index.js

--- pages/Users/index.js
+++
@@ -1,8 +0,0 @@
-import { useSession} from "next-auth/react";
-
-const main = () => {
-    const { data: session, status } = useSession();
-
-    return (<p>{session.user.name}</p>)
-};
-export default main;
