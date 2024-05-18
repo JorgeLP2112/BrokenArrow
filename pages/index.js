108bfffa88e8fd0dd9b6d4e21bf5d67de7e21c38
Jose Kaleb Ruelas Loo <L20550387@chihuahua2.tecnm.mx>
2024-05-18 15:41:26 -0600

    Add redirect in the index page

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,20 @@ import Hero from "@/components/hero";
 import Footer from "@/components/footer";
 import Benefits from "@/components/benefits";
 import { benefitOne, benefitTwo } from "@/components/data";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from 'react';
 
 const Home = () => {
+	const { data: session, status } = useSession();
+	const router = useRouter();
+
+	useEffect(() => {
+		if (session) {
+			router.push('/Publicaciones');
+		}
+	}, [session, router, status]);
+
 	return <BaseLayout>
 		<Hero />
 		<Benefits data={benefitOne} />
