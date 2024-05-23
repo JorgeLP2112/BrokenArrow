8777bc62407e0e736fc664b023ffedf124f4548a
Jose Kaleb Ruelas Loo <L20550387@chihuahua2.tecnm.mx>
2024-05-23 10:04:15 -0600

    Add Login and Singup pages

---
+++ pages/SingUp.js
@@ -0,0 +1,127 @@
+import { useState } from 'react';
+import { signIn } from 'next-auth/react';
+
+function App() {
+    const [role, setRole] = useState('Estudiante');
+    const [error, setError] = useState('');
+
+
+    const handleRegisterGoogle = () => {
+        signIn('google', { callbackUrl: `/api/auth/callback/google?role=${role}` });
+    };
+
+    const handleRegisterLinkedIn = () => {
+        signIn('linkedin', { callbackUrl: `/api/auth/callback/linkedin?role=${role}` });
+    };
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        const email = e.target.email.value
+        const password = e.target.password.value
+        const confirmPassword = e.target.confirmPassword.value
+
+        if (password === confirmPassword) {
+            signIn('credentials', { email, password, callbackUrl: '/api/auth/callback/credentials' })
+        } else {
+            setError('Las contraseñas no coinciden.')
+        }
+
+    }
+
+    return (
+        <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+            <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+                <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+                    <div className="mt-12 flex flex-col items-center">
+                        <h1 className="text-2xl xl:text-3xl font-extrabold">
+                            Únete a "App"
+                        </h1>
+                        <div className="w-full flex-1 mt-8">
+                            <div className="flex flex-col items-center">
+                                <button onClick={handleRegisterGoogle}
+                                    className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
+                                    <div className="bg-white p-2 rounded-full">
+                                        <svg className="w-4" viewBox="0 0 533.5 544.3">
+                                            <path
+                                                d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
+                                                fill="#4285f4" />
+                                            <path
+                                                d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z"
+                                                fill="#34a853" />
+                                            <path
+                                                d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z"
+                                                fill="#fbbc04" />
+                                            <path
+                                                d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z"
+                                                fill="#ea4335" />
+                                        </svg>
+                                    </div>
+                                    <span className="ml-4">
+                                        Registrarse con Google
+                                    </span>
+                                </button>
+
+                                <button onClick={handleRegisterLinkedIn}
+                                    className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5">
+                                    <div className="bg-white p-1 rounded-full">
+                                        <svg className="w-6" viewBox="0 0 48 48">
+                                            <path fill="#0288D1" d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"></path><path fill="#FFF" d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"></path>
+                                        </svg>
+                                    </div>
+                                    <span className="ml-4">
+                                        Registrarse con LinkedIn
+                                    </span>
+                                </button>
+                            </div>
+
+                            <div className="my-12 border-b text-center">
+                                <div
+                                    className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
+                                    O registrate con tu correo
+                                </div>
+                            </div>
+
+                            <div className="mx-auto max-w-xs">
+                                <form onSubmit={handleLogin}>
+                                    <input
+                                        name="email"
+                                        className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                                        type="email" placeholder="Email" />
+                                    <input
+                                        name="password"
+                                        className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                                        type="password" placeholder="Password" />
+                                    <input
+                                        name="confirmPassword"
+                                        className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                                        type="password" placeholder="Confirm Password" />
+                                    <button
+                                        type="submit"
+                                        className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                                        <svg className="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
+                                            stroke-linecap="round" stroke-linejoin="round">
+                                            <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                                            <circle cx="8.5" cy="7" r="4" />
+                                            <path d="M20 8v6M23 11h-6" />
+                                        </svg>
+                                        <span className="ml-3">
+                                            Crear cuenta
+                                        </span>
+                                    </button>
+                                </form>
+                                <p className="mt-12 text-xs text-gray-600 text-center">{error}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex-1 bg-indigo-100 text-center hidden lg:flex">
+                    <div className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
+                        style={{ backgroundImage: "url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg')" }}>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
