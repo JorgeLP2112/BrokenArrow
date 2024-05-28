fc422a778598bb82d1c5a2cb0daff4d0c50c0c13
jorgelp2112 <jorgealexmtz35@gmail.com>
2024-05-27 22:31:49 -0600

    Delete credentials

--- pages/Login.js
+++ pages/Login.js
@@ -5,18 +5,6 @@ function App() {
     signIn("google", { callbackUrl: "/Publicaciones" });
   };
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    const username = e.target.username.value;
-    const password = e.target.password.value;
-
-    signIn("credentials", {
-      username,
-      password,
-      callbackUrl: "/Admins",
-    });
-  };
-
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
       <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
@@ -53,47 +41,6 @@ function App() {
                   </div>
                   <span className="ml-4">Inicia sesión con Google</span>
                 </button>
-              </div>
-
-              <div className="my-12 border-b text-center">
-                <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
-                  O inicia con tu correo
-                </div>
-              </div>
-
-              <div className="mx-auto max-w-xs">
-                <form onSubmit={handleLogin}>
-                  <input
-                    name="username"
-                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                    type="email"
-                    placeholder="Email"
-                  />
-                  <input
-                    name="password"
-                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                    type="password"
-                    placeholder="Password"
-                  />
-                  <button
-                    type="submit"
-                    className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
-                  >
-                    <svg
-                      className="w-6 h-6 -ml-2"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
-                      <circle cx="8.5" cy="7" r="4" />
-                      <path d="M20 8v6M23 11h-6" />
-                    </svg>
-                    <span className="ml-3">Iniciar sesión</span>
-                  </button>
-                </form>
               </div>
             </div>
           </div>
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,8 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@lib/mongodb";
 import { dateNowUnix } from "@/utils/dates";
-import bcrypt from "bcryptjs";
 
 export const authOptions = {
   adapter: MongoDBAdapter(clientPromise),
@@ -20,58 +18,6 @@ export const authOptions = {
           access_type: "offline",
           response_type: "code",
         },
-      },
-    }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "Usuario" },
-        password: {
-          label: "Password",
-          type: "password",
-          placeholder: "Contraseña",
-        },
-      },
-      async authorize(credentials, req) {
-        // Conecta a tu base de datos MongoDB
-        const client = await clientPromise;
-        const db = client.db();
-
-        // Busca el usuario en la colección específica
-        let user = await db
-          .collection("adminUsers")
-          .findOne({ username: credentials.username });
-
-        if (user) {
-          // Compara la contraseña proporcionada con la contraseña hasheada en la base de datos
-          const isMatch = await bcrypt.compare(
-            credentials.password,
-            user.password
-          );
-
-          if (isMatch) {
-            // Si las contraseñas coinciden, devuelve el usuario
-            return { id: user._id, username: user.username };
-          } else {
-            // Si las contraseñas no coinciden, devuelve null
-            return null;
-          }
-        } else {
-          console.log("username:", credentials.username);
-          // Si el usuario no existe, crea un nuevo usuario
-          const hashedPassword = await bcrypt.hash(credentials.password, 10);
-          const result = await db.collection("adminUsers").insertOne({
-            username: credentials.username,
-            password: hashedPassword,
-            role: "Admin",
-            // Agrega cualquier otro campo que necesites aquí
-          });
-
-          console.log("insert result:", result);
-
-          user = result.ops[0];
-          return { id: user._id, username: user.username };
-        }
       },
     }),
   ],
