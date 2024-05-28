38a1971159555c14804d2e0af5b0594c3593e041
BryanSigala <82849413+BryanSigala@users.noreply.github.com>
2024-05-28 00:42:32 -0600

    Changed Students Profile UI

--- components/Navbar.js
+++ components/Navbar.js
@@ -38,7 +38,7 @@ const Navbar = () => {
                     <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
                         {navigation.map((menu, index) => (
                             <li className="mr-3 nav__item" key={index}>
-                                <Link href="/" className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
+                                <Link href="/" className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-300 hover:text-jkb-tertiary focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
                                     {menu}
                                 </Link>
                             </li>
@@ -46,7 +46,7 @@ const Navbar = () => {
                         ))}
                         <li className="mr-3 nav__item">
 
-                            <Link href="/Login" className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5">
+                            <Link href="/Login" className="w-full px-6 py-2 mt-3 text-center text-white bg-jkb-tertiary rounded-md lg:ml-5">
                                 Inicia Sesión
                             </Link>
                         </li>
