41fa6f701b1b881e4d56cc2cd3d20719699119db
Jose Kaleb Ruelas Loo <L20550387@chihuahua2.tecnm.mx>
2024-05-28 20:16:56 -0600

    Add menu for movile

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -39,120 +39,111 @@ const Sidebar = () => {
                 </div>
                 <ul className="sidebar__list">
 
-                    {session ? (
-                        <>
+                    {session ? (<>
+                        <li className="sidebar__item">
+                            <Link
+                                className={`sidebar__link ${router.pathname === "/Publicaciones" ? "sidebar__link--active" : ""
+                                    }`}
+                                href="/Publicaciones"
+                            >
+                                <span className="sidebar__icon">
+                                    <BsPostcardFill />
+                                </span>
+                                <span className="sidebar__name">Publicaciones</span>
+                            </Link>
+                        </li>
+
+                        {session && session.user && session.user.roles && session.user.roles[0] === "Admin" ? (
+                            <>
+                                <li className="sidebar__item">
+                                    <Link
+                                        className={`sidebar__link ${router.pathname === "/Admins/Usuarios" ? "sidebar__link--active" : ""
+                                            }`}
+                                        href="/Admins/Usuarios"
+                                    >
+                                        <span className="sidebar__icon">
+                                            <FaUsers />
+                                        </span>
+                                        <span className="sidebar__name">Usuarios</span>
+                                    </Link>
+                                </li>
+                                <li className="sidebar__item">
+                                    <Link
+                                        className={`sidebar__link ${router.pathname === "/Admins/Reportes" ? "sidebar__link--active" : ""
+                                            }`}
+                                        href="/Admins/Reportes"
+                                    >
+                                        <span className="sidebar__icon">
+                                            <VscReport />
+                                        </span>
+                                        <span className="sidebar__name">Reportes</span>
+                                    </Link>
+                                </li>
+                            </>
+                        ) : (<>
+                            {session && session.user && session.user.roles && session.user.roles[1] === "Estudiante" ? (<>
+                                <li className="sidebar__item">
+                                    <Link
+                                        className={`sidebar__link ${router.pathname === "/Users/Comunidad" ? `sidebar__link--active` : ""
+                                            }`}
+                                        href="/Users/Comunidad"
+                                    >
+                                        <span className="sidebar__icon">
+                                            <BsPeople />
+                                        </span>
+                                        <span className="sidebar__name">Comunidad</span>
+                                    </Link>
+                                </li>
+                            </>) : (<>
+                                <li className="sidebar__item">
+                                    <Link
+                                        className={`sidebar__link ${router.pathname === `/Posts/CrearOferta` ? `sidebar__link--active` : ""
+                                            }`}
+                                        href={`/Posts/CrearOferta`}
+                                    >
+                                        <span className="sidebar__icon">
+                                            <HiDocumentPlus />
+                                        </span>
+                                        <span className="sidebar__name">Nueva oferta</span>
+                                    </Link>
+                                </li>
+                                <li className="sidebar__item">
+                                    <Link
+                                        className={`sidebar__link ${router.pathname === `/Posts/OfertasHechas` ? `sidebar__link--active` : ""
+                                            }`}
+                                        href={`/Posts/OfertasHechas`}
+                                    >
+                                        <span className="sidebar__icon">
+                                            <FaRegFolderOpen />
+                                        </span>
+                                        <span className="sidebar__name">Ofertas hechas</span>
+                                    </Link>
+                                </li>
+                            </>)}
+
                             <li className="sidebar__item">
                                 <Link
-                                    className={`sidebar__link ${router.pathname === "/Publicaciones" ? "sidebar__link--active" : ""
+                                    className={`sidebar__link ${router.pathname === `/Users/${session.user.id}` ? `sidebar__link--active` : ""
                                         }`}
-                                    href="/Publicaciones"
+                                    href={`/Users/${session.user.id}`}
                                 >
                                     <span className="sidebar__icon">
-                                        <BsPostcardFill />
+                                        <CgProfile />
                                     </span>
-                                    <span className="sidebar__name">Publicaciones</span>
+                                    <span className="sidebar__name">Perfil</span>
                                 </Link>
                             </li>
 
-                            {session && session.user && session.user.roles && session.user.roles[0] === "Admin" ? (
-                                <>
-                                    <li className="sidebar__item">
-                                        <Link
-                                            className={`sidebar__link ${router.pathname === "/Admins/Usuarios" ? "sidebar__link--active" : ""
-                                                }`}
-                                            href="/Admins/Usuarios"
-                                        >
-                                            <span className="sidebar__icon">
-                                                <FaUsers />
-                                            </span>
-                                            <span className="sidebar__name">Usuarios</span>
-                                        </Link>
-                                    </li>
-                                    <li className="sidebar__item">
-                                        <Link
-                                            className={`sidebar__link ${router.pathname === "/Admins/Reportes" ? "sidebar__link--active" : ""
-                                                }`}
-                                            href="/Admins/Reportes"
-                                        >
-                                            <span className="sidebar__icon">
-                                                <VscReport />
-                                            </span>
-                                            <span className="sidebar__name">Reportes</span>
-                                        </Link>
-                                    </li>
-                                </>
-                            ) : (
-                                <>
-                                    {session && session.user && session.user.roles && session.user.roles[1] === "Estudiante" ? (
-                                        <>
-                                            <li className="sidebar__item">
-                                                <Link
-                                                    className={`sidebar__link ${router.pathname === "/Users/Comunidad" ? `sidebar__link--active` : ""
-                                                        }`}
-                                                    href="/Users/Comunidad"
-                                                >
-                                                    <span className="sidebar__icon">
-                                                        <BsPeople />
-                                                    </span>
-                                                    <span className="sidebar__name">Comunidad</span>
-                                                </Link>
-                                            </li>
-                                        </>
-                                    ) : (
-                                        <>
-                                            <li className="sidebar__item">
-                                                <Link
-                                                    className={`sidebar__link ${router.pathname === `/Posts/CrearOferta` ? `sidebar__link--active` : ""
-                                                        }`}
-                                                    href={`/Posts/CrearOferta`}
-                                                >
-                                                    <span className="sidebar__icon">
-                                                        <HiDocumentPlus />
-                                                    </span>
-                                                    <span className="sidebar__name">Nueva oferta</span>
-                                                </Link>
-                                            </li>
-                                            <li className="sidebar__item">
-                                                <Link
-                                                    className={`sidebar__link ${router.pathname === `/Posts/OfertasHechas` ? `sidebar__link--active` : ""
-                                                        }`}
-                                                    href={`/Posts/OfertasHechas`}
-                                                >
-                                                    <span className="sidebar__icon">
-                                                        <FaRegFolderOpen />
-                                                    </span>
-                                                    <span className="sidebar__name">Ofertas hechas</span>
-                                                </Link>
-                                            </li>
-                                        </>
-                                    )}
-
-                                    <li className="sidebar__item">
-                                        <Link
-                                            className={`sidebar__link ${router.pathname === `/Users/${session.user.id}` ? `sidebar__link--active` : ""
-                                                }`}
-                                            href={`/Users/${session.user.id}`}
-                                        >
-                                            <span className="sidebar__icon">
-                                                <CgProfile />
-                                            </span>
-                                            <span className="sidebar__name">Perfil</span>
-                                        </Link>
-                                    </li>
-
-                                </>
-
-                            )}
-                            <li className="sidebar__item__SignOut">
-                                <button className="sidebar__link" onClick={() => signOut({ callbackUrl: '/' })}>
-                                    <span className="sidebar__icon">
-                                        <CiLogout />
-                                    </span>
-                                    <span className="sidebar__name">Sign Out</span>
-                                </button>
-                            </li>
-                        </>
-                    ) : (null)}
+                        </>)}
+                        <li className="sidebar__item__SignOut">
+                            <button className="sidebar__link" onClick={() => signOut({ callbackUrl: '/' })}>
+                                <span className="sidebar__icon">
+                                    <CiLogout />
+                                </span>
+                                <span className="sidebar__name">Sign Out</span>
+                            </button>
+                        </li>
+                    </>) : (null)}
                 </ul>
             </aside>
         </div>
